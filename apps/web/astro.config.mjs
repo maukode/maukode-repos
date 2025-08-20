43cda5db4ac5a242902250d8d4a52124bf45c55a
Ucok23 <ulubis98@gmail.com>
2025-08-20 15:45:47 +0700

    update maukode web and docs

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -2,27 +2,52 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import Icons from 'unplugin-icons/vite';
+import starlightSidebarTopics from 'starlight-sidebar-topics';
+import starlightBlog from 'starlight-blog'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: '@maukode',
+			title: 'MauKode',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Headless Media',
-					items: [
-						{ label: 'Getting Started', autogenerate: {directory: '/headless-media/getting-started/'} },
-						{ label: 'Frameworks', autogenerate: { directory: '/headless-media/frameworks/' } }
+			plugins: [
+				starlightSidebarTopics(
+					[
+						// A topic representing a guide section of your project.
+						{
+							label: 'Headless Media',
+							// The page to link to when the topic is clicked.
+							link: '/headless-media/',
+							// The sidebar configuration for the topic.
+							items: [
+								{
+									label: 'Getting Started',
+									autogenerate: { directory: 'headless-media/getting-started/' }
+								},
+								{
+									label: 'Frameworks',
+									autogenerate: { directory: 'headless-media/frameworks/' }
+								},
+							],
+						}
 					],
-				}
+					{
+						exclude: ['/blog', '/blog/*']
+					}
+				),
+				starlightBlog()
 			],
+			favicon: '/favicon.ico',
+			logo: {
+				src: './src/assets/maukode-logo.svg',
+				alt: 'Maukode Logo'
+			}
 		}),
 	],
 	vite: {
 		plugins: [
-			Icons({compiler: 'astro'})
+			Icons({ compiler: 'astro' })
 		]
 	}
 });
---
+++ apps/web/src/content/docs/blog/welcome-to-maukode.md
@@ -0,0 +1,8 @@
+---
+title: Welcome to maukode
+date: 2023-07-24
+---
+
+## Hello
+
+Hello world!
